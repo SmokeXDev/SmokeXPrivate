a7f170475e14c5138f4dff60f0f884fba5dbff35
SmokeX <111679379+SmokeXDev@users.noreply.github.com>
2023-02-12 23:29:32 +0100

    Delete StuffLoader.lua

--- StuffLoader.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/Loader.lua", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPrivate/main/NameTag.lua", true))()
