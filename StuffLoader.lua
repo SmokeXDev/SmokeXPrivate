2ab28108c5d1a013aa1b566a52ae0b659dff849f
SmokeX <111679379+SmokeXDev@users.noreply.github.com>
2023-02-12 23:06:59 +0100

    Create StuffLoader.lua

---
+++ StuffLoader.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/Loader.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPrivate/main/NameTag.lua", true))()
